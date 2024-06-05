b9d6a02db50500cb3f90463f62ca6d321fc9ed30
ropehapi <ropehapi@gmail.com>
2024-06-05 15:17:34 -0300

    Setup de configurações do projeto com viper e conexão com DB

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"fmt"
+	"github.com/ropehapi/desafio-stone/configs"
+)
 
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", configs.DBUser, configs.DBPass, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
